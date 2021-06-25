6a38be8d23fb2300659655f9db96b5cd63997a21
Musa Aytmamatov <black.nobody.anb@gmail.com>
2021-06-25 13:03:44 +0600

    layout project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,74 @@
+//styles
 import "./sass/base.sass";
 
+import CardImage1 from "./assets/Audio Speacker.png";
+import CardImage2 from "./assets/headphones.png";
+import CardImage3 from "./assets/drum_cut.jpeg";
+import CardImage4 from "./assets/Rocket.png";
+import CardImage5 from "./assets/Microphone.png";
+import CardImage6 from "./assets/Bomb.png";
+import CardImage7 from "./assets/Tv.png";
+
+// component
 import Card from "./components/Card/Card";
 
 const cards = [
-  { title: "Audio Speaker", desc: "Audio", price: "$299" },
-  { title: "Headphones", desc: "Audio", price: "$199" },
-  { title: "Brownies", desc: "Classic Shoes", price: "$599" },
-  { title: "Rocket", desc: "Polo T-shirt", price: "$39" },
-  { title: "Hoodie", desc: "Clothes", price: "$139" },
-  { title: "Retro Eye", desc: "Glasses", price: "$399" },
-  { title: "Military Pants", desc: "Pants", price: "$199" },
+  {
+    title: "Audio Speaker",
+    desc: "Audio",
+    price: "$299",
+    pathImage: CardImage1,
+    id: "1",
+    styles: { isDark: false, isCenter: false, padding: true, isBackgroundImage: false },
+  },
+  {
+    title: "Headphones",
+    desc: "Audio",
+    price: "$199",
+    pathImage: CardImage2,
+    id: "2",
+    styles: { isDark: true, isCenter: true, padding: false, isBackgroundImage: false },
+  },
+  {
+    title: "Brownies",
+    desc: "Classic Shoes",
+    price: "$599",
+    pathImage: CardImage3,
+    id: "3",
+    styles: { isDark: false, isCenter: true, padding: false, isBackgroundImage: false },
+  },
+  {
+    title: "Rocket",
+    desc: "Polo T-shirt",
+    price: "$39",
+    pathImage: CardImage4,
+    id: "4",
+    styles: { isDark: false, isCenter: false, padding: true, isBackgroundImage: false },
+  },
+  {
+    title: "Hoodie",
+    desc: "Clothes",
+    price: "$139",
+    pathImage: CardImage5,
+    id: "5",
+    styles: { isDark: false, isCenter: false, padding: false, isBackgroundImage: false },
+  },
+  {
+    title: "Retro Eye",
+    desc: "Glasses",
+    price: "$399",
+    pathImage: CardImage6,
+    id: "6",
+    styles: { isDark: false, isCenter: false, padding: false, isBackgroundImage: false },
+  },
+  {
+    title: "Military Pants",
+    desc: "Pants",
+    price: "$199",
+    pathImage: CardImage7,
+    id: "7",
+    styles: { isDark: false, isCenter: false, padding: false, isBackgroundImage: true },
+  },
 ];
 
 function App() {
@@ -18,7 +77,19 @@ function App() {
       <div className="container">
         <div className="cards">
           {cards.map((card) => {
-            return <Card title={card.title} desc={card.desc} price={card.price} />;
+            return (
+              <Card
+                title={card.title}
+                key={card.id}
+                padding={card.styles.padding}
+                src={card.pathImage}
+                desc={card.desc}
+                price={card.price}
+                isDark={card.styles.isDark}
+                isCenter={card.styles.isCenter}
+                // isBackgroundImage={card.styles.isBackgroundImage}
+              />
+            );
           })}
         </div>
       </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
-function Card({ title, desc, price }) {
+// image
+import cardImage from "./../../assets/Audio Speacker.png";
+
+// style
+import "./card.sass";
+
+function Card({ title, desc, price, src = cardImage, padding, isDark, isCenter, isBackgroundImage }) {
   return (
-    <div className="card">
-      <h2 className="card__title">{title}</h2>
-      <span className="card__desc">{desc}</span>
-      <span className="card__price">{price}</span>
+    <div className="card" style={isDark ? { background: "#2C2C2C" } : null}>
+      <div className="card__img" style={padding || isBackgroundImage ? { padding: "17px 25px" } : null}>
+        <img src={src} alt="card__image" />
+      </div>
+      <div className="card__inner" style={isCenter ? { textAlign: "center" } : null}>
+        <h2 className="card__title" style={isDark ? { color: "#fff" } : null}>
+          {title}
+        </h2>
+        <div className="card__info" style={isCenter ? { flexDirection: "column" } : null}>
+          <span className="card__desc" style={isCenter ? { marginBottom: "18px" } : null}>
+            {desc}
+          </span>
+          <span className="card__price" style={isDark ? { color: "#fff" } : null}>
+            {price}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
